54537540083509db932b596a6c5b32b227354436
Nikola Kušen <nikola.kusen@gmail.com>
2023-11-01 13:04:01 +0100

    TAG-11 Dodati osnovni frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import UserList from "./components/UserList.jsx";
+
+function App() {
+  return (
+    <div>
+      <UserList/>
+    </div>
+  )
+}
+
+export default App
