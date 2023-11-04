e0138fae3a00ebbbc0e1f7201f0e60de97e44f3b
Yjuhu <jakov.sarolic.1@gmail.com>
2023-11-04 23:00:17 +0100

    TAG-20 Napravio djelomicno .jsx za login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import UserList from "./components/UserList.jsx";
+import Login from './components/Login.jsx';
 
 function App() {
   return (
     <div>
-      <UserList/>
+      <Login />
     </div>
   )
 }
