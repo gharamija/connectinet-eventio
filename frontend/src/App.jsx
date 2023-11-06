d123b15fb243e3ccd013f1f445f78e5da7263665
Yjuhu <jakov.sarolic.1@gmail.com>
2023-11-06 03:10:40 +0100

    TAG-20 Napravio Login.jsx i Register.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import UserList from "./components/UserList.jsx";
 import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   )
 }
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import './Register.css';
+
+function Register(props) {
+    const [registerForm, setRegisterForm] = React.useState({ username: '', email: '', password: '', type: '', naziv: '', adresa: '', poveznica: '', clanarina: '' });
+    const [error, setError] = React.useState('');
+    const [isChecked, setIsChecked] = useState(false);
+    const [isPaid, setIsPaid] = useState(false);
+
+
+
+    function onChange(event) {
+        const { name, value, type, checked } = event.target;
+        if (type === 'checkbox' && name === 'admin') {
+            setRegisterForm(oldForm => ({ ...oldForm, [name]: checked }))
+            setIsChecked(checked);
+        } else if (type === 'checkbox' && name === 'clanarina') {
+            setRegisterForm(oldForm => ({ ...oldForm, [name]: checked }))
+            setIsPaid(checked);
+        } else {
+            setRegisterForm(oldForm => ({ ...oldForm, [name]: value }))
+        }
+    }
+
+    function onSubmit(e) {
+        e.preventDefault();
+        setError("");
+        if (isChecked) {
+            const body = `username=${registerForm.username}&email=${registerForm.email}&password=${registerForm.password}&type=${"ORGANIZATOR"}&naziv=${registerForm.naziv}&adresa=${registerForm.adresa}&poveznica=${registerForm.poveznica}&članarina=${registerForm.clanarina}`;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: body
+            };
+            fetch('/api/organizator/register', options)
+                .then(response => {
+                    if (response.status === 400) {
+                        setError(response.statusText);
+                    } else {
+                        props.onRegister();
+                    }
+                });
+        } else {
+            const body = `username=${registerForm.username}&email=${registerForm.email}&password=${registerForm.password}&type=${"POSJETITELJ"}`;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: body
+            };
+            fetch('/api/user/register', options)
+                .then(response => {
+                    if (response.status === 400) {
+                        setError(response.statusText);
+                    } else {
+                        props.onRegister();
+                    }
+                });
+        }
+    }
+
+    return (
+        <div className="Register">
+            <form onSubmit={onSubmit}>
+                <div className="FormRow">
+                    <label>Username</label>
+                    <input name='username' onChange={onChange} value={registerForm.username} />
+                </div>
+                <div className="FormRow">
+                    <label>Email</label>
+                    <input name='email' onChange={onChange} value={registerForm.email} />
+                </div>
+                <div className="FormRow">
+                    <label>Password</label>
+                    <input name='password' type="password" onChange={onChange} value={registerForm.password} />
+                </div>
+                <div className='FormRow'>
+                    <label>
+                        Organiser role
+                        <input
+                            type="checkbox"
+                            name='admin'
+                            checked={isChecked}
+                            onChange={onChange}
+                        />
+                    </label>
+                </div>
+                {isChecked && (
+                    <div className="AddInfo">
+                        <div className="FormRow">
+                            <label>Name</label>
+                            <input name='naziv' onChange={onChange} value={registerForm.naziv} />
+                        </div>
+                        <div className="FormRow">
+                            <label>Address</label>
+                            <input name='adresa' onChange={onChange} value={registerForm.adresa} />
+                        </div>
+                        <div className="FormRow">
+                            <label>Link</label>
+                            <input name='poveznica' onChange={onChange} value={registerForm.poveznica} />
+                        </div>
+                        <div className="FormRow">
+                            <label>Subscription</label>
+                            <input type='checkbox' name='clanarina' onChange={onChange} checked={isPaid} />
+                        </div>
+                    </div>
+                )}
+                <div className='error'>{error}</div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
