3b55fa6cced3683ab9effab88fbf8e0fd56bc70a
Gašpar Haramija <gaspar.haramija@gmail.com>
2023-11-15 15:21:07 +0100

    TAG-23 front pre-final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: import.meta.env.API_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
