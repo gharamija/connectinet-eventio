fc6bfb6892c393a4a0180a2c6eae40d948bdfbe8
Sandro Boka <135635545+SandroBoka@users.noreply.github.com>
2023-12-30 19:37:44 +0100

    TAG-48 Tablice za prikaz korisnika
    * TAG-48 Dodana pocetna tablica u kojoj ce biti korisnici
    
    * TAG-48 Dodana pocetna tablica u kojoj ce biti korisnici
    
    * TAG-48 Tablica promijenjena da sadrzi podatke za korisnike
    
    * TAG-48 Tablica promijenjena da sadrzi podatke za korisnike i za organizatore
    
    * TAG-48 Dodani gumbi za prikaz ili posjetitelja ili organizatora, dodano i brisanje, ali nije spojeno na backend

--- frontend/src/components/User.jsx
+++
@@ -1,8 +0,0 @@
-function User(props) {
-  const { id, username } = props.user;
-    return (
-        <p>{id} {username}</p>
-    );
-}
-
-export default User;
--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,33 +1,63 @@
 import * as React from "react";
-import User from "./User.jsx";
-import { Box } from "@mui/material";
+import {Box, Button, Grid} from "@mui/material";
+import EnhancedTable from "./TableComponents/EnhancedTable";
+import {useState} from "react";
 
 function UserList() {
-  const [isAllowed, setIsAllowed] = React.useState(false);
-  const [users, setUsers] = React.useState([]);
+    const [isAllowed, setIsAllowed] = React.useState(false);
+    const [users, setUsers] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch("api/user/all").then((response) => {
-      if (response.ok) {
-        setIsAllowed(true);
-        response.json().then((users) => setUsers(users));
-      }
-    });
-  }, []);
+    React.useEffect(() => {
+        fetch("api/user/all").then((response) => {
+            if (response.ok) {
+                setIsAllowed(true);
+                response.json().then((users) => setUsers(users));
+            }
+        });
+    }, []);
 
-  if (isAllowed) {
-    return (
-      <Box>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{<User user={user} />}</li>
-          ))}
-        </ul>
-      </Box>
-    );
-  } else {
-    return <Box>Ne smijete vidjeti sve korisnike</Box>;
-  }
+    const [posjetiteljiButton, setPosjetiteljiButton] = useState(true);
+    const [organizatoriButton, setOrganizatoriButton] = useState(false);
+
+    const handleButtonClick = (button) => {
+        if (button === "posjetitelji") {
+            setPosjetiteljiButton(true);
+            setOrganizatoriButton(false);
+        } else if (button === "organizatori") {
+            setPosjetiteljiButton(false);
+            setOrganizatoriButton(true);
+        }
+    };
+
+    if (isAllowed) {
+        return (
+            <>
+                <Box sx={{my: 2, mx: 1, marginBottom: 5, marginTop: 5}}></Box>
+                <div>
+                    <Grid container spacing={2} justifyContent="center">
+                        <Grid item>
+                            <Button variant={posjetiteljiButton ? "contained" : "outlined"} color="primary"
+                                    onClick={() => handleButtonClick("posjetitelji")}>
+                                Posjetitelji
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant={organizatoriButton ? "contained" : "outlined"} color="primary"
+                                    onClick={() => handleButtonClick("organizatori")}>
+                                Organizatori
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
+
+                {posjetiteljiButton && <EnhancedTable users={users} role={"POSJETITELJ"}/>}
+                {organizatoriButton && <EnhancedTable users={users} role={"ORGANIZATOR"}/>}
+            </>
+    )
+        ;
+    } else {
+        return <Box>Ne smijete vidjeti sve korisnike</Box>;
+    }
 }
 
 export default UserList;
